8838a9c623682e9a11e60dca79d6514886718934
eddiebary <edgarmonis@gmail.com>
2020-09-07 14:25:13 +0000

    updating submodules

--- run_search_process_test.py
+++ run_search_process_test.py
@@ -33,7 +33,7 @@ boosting_tokens = keyword_extractor.parse_regex_query(query_string)
 print(boosting_tokens)
 
 # Create a lucene query
-QueryGen = QueryGenerator(StandardAnalyzer(), use_synonyms=True)
+QueryGen = QueryGenerator(StandardAnalyzer(), use_synonyms=False)
 query = QueryGen.build_query(query_string, \
     boosting_tokens, "OR_QUERY", field="Master Question")
 
