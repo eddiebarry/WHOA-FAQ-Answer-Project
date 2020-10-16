c2ec9e8282f0f7279f4f267e7c771d58d76a9d66
eddiebarry <edgarmonis@gmail.com>
2020-10-16 07:35:42 +0000

    changing code test

--- app_test.py
+++ app_test.py
@@ -7,49 +7,6 @@ import timeit
 from statistics import mean 
 # TODO : Use Pytest
 
-# """
-# Single Keyword Extract Test
-# """
-# base_url="http://0.0.0.0:5007/api/v2/keyword_extract"
-# params = {
-#     "question":"My child is sick where can he get measles vaccine ?",
-#     "answer":"Please go to khandala",
-# }
-
-# r = requests.get(base_url, params=params)
-# print(r.text)
-# """
-# {
-#     "Disease 1": [
-#         "measles"
-#     ], 
-#     "Disease 2": [
-#         "measles"
-#     ], 
-#     "Keyword": [
-#         "child", 
-#         "measles"
-#     ], 
-#     "Other -condition, symptom etc": [
-#         "sick", 
-#         "child"
-#     ], 
-#     "Subject - Person": [
-#         "child"
-#     ], 
-#     "Vaccine 1": [
-#         "measles"
-#     ], 
-#     "Vaccine 2": [
-#         "measles"
-#     ], 
-#     "Who is writing this": [
-#         "child"
-#     ]
-# }
-# """
-
-
 """
 Batch Keyword Extract Test
 """
@@ -223,129 +180,6 @@ example response
 
 
 """
-{
-  "questions_keywords_dict": [
-    "30f05b0d8b1d20dde20fd58ac8bc07e9ae1e664eefeada3d843d48dbe653afec1df8116a1755fb4a956166ecd67c61103f9fea5a9deb14ed1b551e80eecaeb5b": {
-      "Disease 1": [
-        "measles"
-      ], 
-      "Disease 2": [
-        "measles"
-      ], 
-      "Keyword": [
-        "child", 
-        "measles", 
-        "immunize", 
-        "immunization"
-      ], 
-      "Other -condition, symptom etc": [
-        "sick", 
-        "child", 
-        "baby"
-      ], 
-      "Subject - Person": [
-        "child", 
-        "baby"
-      ], 
-      "Subject 1 - Immunization": [
-        "immunization", 
-        "vaccination"
-      ], 
-      "Subject 2 - Vaccination / General": [
-        "travel", 
-        "vaccination"
-      ], 
-      "Vaccine 1": [
-        "measles"
-      ], 
-      "Vaccine 2": [
-        "measles"
-      ], 
-      "Who is writing this": [
-        "child"
-      ]
-    }, 
-    "3b454a1b958d0abe190821768e21a837be283e02ff2c6a2b30d2f5363c1275221aabb35aa23319fc0ae24a33f022f8664b5a93d5eb0659c2eb1e1b0a2ae66383": {
-      "Disease 2": [
-        "polio"
-      ], 
-      "Keyword": [
-        "child", 
-        "immunize", 
-        "immunization"
-      ], 
-      "Other -condition, symptom etc": [
-        "sick", 
-        "child", 
-        "baby"
-      ], 
-      "Subject - Person": [
-        "child", 
-        "baby"
-      ], 
-      "Subject 1 - Immunization": [
-        "immunization", 
-        "vaccination"
-      ], 
-      "Subject 2 - Vaccination / General": [
-        "travel", 
-        "vaccination"
-      ], 
-      "Vaccine 1": [
-        "polio"
-      ], 
-      "Vaccine 2": [
-        "polio"
-      ], 
-      "Who is writing this": [
-        "child"
-      ]
-    }, 
-    "74ef7552dbf54d910a22174b0905b5965be9d2644cbb4d68406c9a23d128d20df8f6386106e8a7fdd7dddec549a204a54089887e89d0a86bf7cfca1ab2ba0ad8": {
-      "Disease 1": [
-        "rubella"
-      ], 
-      "Disease 2": [
-        "rubella"
-      ], 
-      "Keyword": [
-        "child", 
-        "immunize", 
-        "immunization"
-      ], 
-      "Other -condition, symptom etc": [
-        "sick", 
-        "child", 
-        "baby"
-      ], 
-      "Subject - Person": [
-        "child", 
-        "baby"
-      ], 
-      "Subject 1 - Immunization": [
-        "immunization", 
-        "vaccination"
-      ], 
-      "Subject 2 - Vaccination / General": [
-        "travel", 
-        "vaccination"
-      ], 
-      "Vaccine 1": [
-        "rubella"
-      ], 
-      "Vaccine 2": [
-        "rubella"
-      ], 
-      "Who is writing this": [
-        "child"
-      ]
-    }
-  ]
-}
-"""
-
-
-"""
 Batch QA index 
 """
 
@@ -376,6 +210,14 @@ base_url="http://0.0.0.0:5007/api/v2/train_bot_json_array"
 r = requests.post(base_url, data=json.dumps(data))
 print(r.text)
 
+
+
+
+
+
+"""
+Reranking timer test
+"""
 base_url="http://18.203.115.216:5007/api/v1/reranking"
 
 gpu_times_dict = {}
@@ -444,6 +286,10 @@ for x in sorted(os.listdir(dir_)):
 print('$'*80)
 print("reranking service done")
 
+
+"""
+QUESTION ASKER TIMING TEST
+"""
 
 base_url="http://0.0.0.0:5007/api/v2/qna"
 
