1cb90c73ab8fe211fc5bba1c34dc148a7b8aa941
eddiebarry <edgarmonis@gmail.com>
2020-11-09 16:05:41 +0000

    Changing the test to point to local

--- app_test.py
+++ app_test.py
@@ -150,33 +150,33 @@ example response
   ]
 }
 """
-# qa_list = [
-#     {
-#         "question":"My child is sick where can he get measles vaccine ?",
-#         "answer":"Please go to khandala",
-#     },
-#     {
-#         "question":"My child is sick where can he get rubella vaccine ?",
-#         "answer":"Please go to khandala",
-#     },
-#     {
-#         "question":"My child is sick where can he get polio vaccine ?",
-#         "answer":"Please go to khandala",
-#     },
-# ]
+qa_list = [
+    {
+        "question":"My child is sick where can he get measles vaccine ?",
+        "answer":"Please go to khandala",
+    },
+    {
+        "question":"My child is sick where can he get rubella vaccine ?",
+        "answer":"Please go to khandala",
+    },
+    {
+        "question":"My child is sick where can he get polio vaccine ?",
+        "answer":"Please go to khandala",
+    },
+]
 
-# for x in qa_list:
-#     query_string = x['question']+x['answer']
-#     qa_hash = hashlib.sha512(query_string.encode()).hexdigest()
-#     x['id']=qa_hash
+for x in qa_list:
+    query_string = x['question']+x['answer']
+    qa_hash = hashlib.sha512(query_string.encode()).hexdigest()
+    x['id']=qa_hash
 
-# batch_response_test = {
-#     "question_answer_list" : qa_list
-# }
+batch_response_test = {
+    "question_answer_list" : qa_list
+}
 
-# base_url = url  + "/api/v2/batch_keyword_extract"
-# r = requests.post(base_url, data=json.dumps(batch_response_test))
-# print(r.text)
+base_url = url  + "/api/v2/batch_keyword_extract"
+r = requests.post(base_url, data=json.dumps(batch_response_test))
+print(r.text)
 
 
 """
@@ -377,6 +377,119 @@ base_url= url + "/api/v2/train_bot_json_array"
 
 r = requests.post(base_url, data=json.dumps(data))
 print(r.text)
+
+
+data = {
+    "project_id":999,
+    "version_id":3,
+    "previous_versions" : [
+      1,
+      2
+    ]
+}
+questions = []
+# dir_ = "./tests/test_data/vsn_data"
+dir_ = "./tests/intermediate_results/vsn_data_variations"
+protected = [
+        'question','answer','question_variation_0',
+        'question_variation_1','question_variation_2', 'id'
+    ]
+for idx,x in enumerate(sorted(os.listdir(dir_))):
+    if x.endswith(".json"):
+        jsonpath = os.path.join(dir_,x)
+        f = open(jsonpath,)
+        jsonObj = json.load(f)
+        jsonObj['id']=str(idx)
+        f.close()
+        keywords = []
+        to_remove = []
+        for x in jsonObj.keys():
+            if x in protected:
+                continue
+            if jsonObj[x] != "":
+                new_dict = {
+                    x: [jsonObj[x]]
+                }
+                keywords.append(new_dict)
+            to_remove.append(x)
+        for x in to_remove:    
+            jsonObj.pop(x,None)
+        jsonObj['keywords']=keywords
+        questions.append(jsonObj)
+
+keyword_dir_path = "./tests/unique_keywords.json"
+f = open(keyword_dir_path,)
+keyword_directory = json.load(f)
+
+data['question_list'] = questions
+
+new_dir = []
+for x in keyword_directory:
+  new_dir.append({x:keyword_directory[x]})
+data['keyword_directory'] = new_dir
+
+base_url= url + "/api/v2/train_bot_json_array"
+
+r = requests.post(base_url, data=json.dumps(data))
+print(r.text)
+print(len(questions), "is the number of questions added")
+
+
+data = {
+    "project_id":999,
+    "version_id":4,
+    "previous_versions" : [
+      1,
+      2,
+      3
+    ]
+}
+questions = []
+# dir_ = "./tests/test_data/vsn_data"
+dir_ = "./tests/intermediate_results/vsn_data_variations"
+protected = [
+        'question','answer','question_variation_0',
+        'question_variation_1','question_variation_2', 'id'
+    ]
+for idx,x in enumerate(sorted(os.listdir(dir_))):
+    if x.endswith(".json"):
+        jsonpath = os.path.join(dir_,x)
+        f = open(jsonpath,)
+        jsonObj = json.load(f)
+        jsonObj['id']=str(idx)
+        f.close()
+        keywords = []
+        to_remove = []
+        for x in jsonObj.keys():
+            if x in protected:
+                continue
+            if jsonObj[x] != "":
+                new_dict = {
+                    x: [jsonObj[x]]
+                }
+                keywords.append(new_dict)
+            to_remove.append(x)
+        for x in to_remove:    
+            jsonObj.pop(x,None)
+        jsonObj['keywords']=keywords
+        questions.append(jsonObj)
+
+keyword_dir_path = "./tests/unique_keywords.json"
+f = open(keyword_dir_path,)
+keyword_directory = json.load(f)
+
+data['question_list'] = questions
+
+new_dir = []
+for x in keyword_directory:
+  new_dir.append({x:keyword_directory[x]})
+data['keyword_directory'] = new_dir
+
+base_url= url + "/api/v2/train_bot_json_array"
+
+r = requests.post(base_url, data=json.dumps(data))
+print(r.text)
+print(len(questions), "is the number of questions added")
 pdb.set_trace()
 
 # """
@@ -407,56 +520,6 @@ pdb.set_trace()
 #             stop = timeit.default_timer()
 #             times.append(stop-start)
 # print(mean(times))
-# print(r.text)
-
-# data = {
-#     "project_id":999,
-#     "version_id":2,
-# }
-# questions = []
-# # dir_ = "./tests/test_data/vsn_data"
-# dir_ = "./tests/intermediate_results/vsn_data_variations"
-# protected = [
-#         'question','answer','question_variation_0',
-#         'question_variation_1','question_variation_2', 'id'
-#     ]
-# for idx,x in enumerate(sorted(os.listdir(dir_))):
-#     if x.endswith(".json"):
-#         jsonpath = os.path.join(dir_,x)
-#         f = open(jsonpath,)
-#         jsonObj = json.load(f)
-#         jsonObj['id']=str(idx)
-#         f.close()
-#         keywords = []
-#         to_remove = []
-#         for x in jsonObj.keys():
-#             if x in protected:
-#                 continue
-#             if jsonObj[x] != "":
-#                 new_dict = {
-#                     x: [jsonObj[x]]
-#                 }
-#                 keywords.append(new_dict)
-#             to_remove.append(x)
-#         for x in to_remove:    
-#             jsonObj.pop(x,None)
-#         jsonObj['keywords']=keywords
-#         questions.append(jsonObj)
-
-# keyword_dir_path = "./tests/unique_keywords.json"
-# f = open(keyword_dir_path,)
-# keyword_directory = json.load(f)
-
-# data['question_list'] = questions
-
-# new_dir = []
-# for x in keyword_directory:
-#   new_dir.append({x:keyword_directory[x]})
-# data['keyword_directory'] = new_dir
-
-# base_url= url + "/api/v2/train_bot_json_array"
-
-# r = requests.post(base_url, data=json.dumps(data))
 # print(r.text)
 
 # """
