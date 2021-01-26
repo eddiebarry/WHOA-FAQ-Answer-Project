a1cee56ba1ef5f6c7921823f031a41c11ef06083
eddiebarry <edgarmonis@gmail.com>
2021-01-26 07:05:28 +0000

    updating the app load test

--- load-test/load_test_app.py
+++ load-test/load_test_app.py
@@ -14,13 +14,6 @@ inp = [
     {'query': 'She is 6 years old', 'user_id': '768930bf736e05cc1c5609a91d9a7bff33493011c2576b7696a4ff4b676b079968d369178ea55c52c9c2f23ff87ed10ba0923c5fb63583d76aa605d826c2306b'}
 ]
 
-def gentr_fn(alist):
-    while 1:
-        for j in alist:
-            yield j
-            
-inp_gen = gentr_fn(inp)
-
 class SearchEngineUser(HttpUser):
     wait_time = between(1, 2)
 
@@ -28,11 +21,14 @@ class SearchEngineUser(HttpUser):
     def index(self):
         
         past_id = '-1'
-        x = inp_gen.__next__()
-        if x['user_id'] == '-1':
-            # pdb.set_trace()
-            resp = self.client.get('/api/v2/qna', data=json.dumps(x)).json()
-            past_id = resp['user_id']
-        else:
-            x['user_id']=past_id
-            resp = self.client.get('/api/v2/qna', data=json.dumps(x)).json()
+        for x in inp:
+            if x['user_id'] == '-1':
+                # pdb.set_trace()
+                resp = self.client.get('/api/v2/qna', data=json.dumps(x)).json()
+                past_id = resp['user_id']
+            else:
+                x['user_id']=past_id
+                resp = self.client.get('/api/v2/qna', data=json.dumps(x)).json()
+
+                if x['query']=='She is 6 years old':
+                    assert resp['what_to_say']['question_0_variation_0']=='Is it possible to only get the measles vaccine without the mumps, rubella and varicella portion for a 6 year old? He has had very bad reactions to other vaccines.'
