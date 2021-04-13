e74edc29a8104865afa6ac6615ffbfa1872d2ef6
eddiebarry <edgarmonis@gmail.com>
2021-04-13 00:05:49 +0000

    fixing jenkins

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lxp-search',
-      version='1.1.2',
+      version='1.0.0',
       packages=find_packages(),
       install_requires=[
           'tokenizers==0.7',
