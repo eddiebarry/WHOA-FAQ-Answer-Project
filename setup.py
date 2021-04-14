68d0cc0cc2371e4aef6562d9980008a65893d54f
eddiebarry <edgarmonis@gmail.com>
2021-04-14 16:47:15 +0000

    updating charts

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='orchestrator',
-      version='1.0.0',
+      version='1.0.1',
       packages=find_packages(),
       package_data={'': ['./nginx-orchestrator-setup/Dockerfile']},
       include_package_data = True,
