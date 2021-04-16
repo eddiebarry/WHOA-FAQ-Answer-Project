b95d5660646ec2774c60d3996529657cbc24a0c1
eddiebarry <edgarmonis@gmail.com>
2021-04-16 12:33:41 +0000

    updating values because the resources already exist

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='vla-orchestrator',
-      version='1.1.0',
+      version='1.1.1',
       packages=find_packages(),
       install_requires=[
       ])
