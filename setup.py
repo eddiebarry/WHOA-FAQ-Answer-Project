91fb3a8f2be045d21f15751510e6fe5fae84fa10
eddiebarry <edgarmonis@gmail.com>
2021-04-21 06:14:36 +0000

    adding labels so that resources can be removed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='orchestrator-server',
-      version='1.1.2',
+setup(name='vla',
+      version='0.0.0',
       packages=find_packages(),
       install_requires=[
       ])
